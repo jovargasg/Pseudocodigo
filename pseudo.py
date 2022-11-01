c408cd8f0086fd9e3b4af5150d47ce1c11db561e
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-11-01 13:08:41 -0300

    Create pseudo.py

---
+++ pseudo.py
@@ -0,0 +1,38 @@
+class Product:
+{
+    ##Atributos##
+    promo=''                        ##Si tiene promociones
+    types_of_drinks=''              ##Tipo de bebestibles
+    types_of_alcohol=''             ##Tipo de alcoholes
+    stock=''                        ##Stock de la tienda
+    snacks=''                       ##Comestible
+    barcode=''                      ##Codigo de barra
+    soft_drink=''                   ##Bebidas gaseosas
+    ice_cream=''                    ##Helado
+    ##Fin de Atributos##
+
+    ##Metodos##
+    update_stock=()             ##Actualizar datos de stock
+    update_snacks=()            ##Actualizar datos de los snacks
+    update_soft_drink=()        ##Actualizar datos de las gaseosas
+    show_promo=()               ##Mostrar promo
+    ##Fin de Metodos##
+}
+  
+class Worker:
+{
+    ##Atributos##
+    name=''                     ##Nombre
+    last_name=''                ##Apellido
+    address=''                  ##Direccion
+    phone=''                    ##Telefono
+    run=''                      ##run
+    birth_day=''                ##Fecha de Nacimiento
+    ##Fin de Atributos##
+
+    ##Metodos##
+    calculate_age=()            ##Calcular la edad
+    show_number_phone=()        ##Mostrar numero de telefono
+    show_data=()                ##Mostrar Datos
+    ##Fin de Metodos##
+}
